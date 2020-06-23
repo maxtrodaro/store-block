1df7c0f7fc1d4e7356559a279b1088f071891ae4
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-23 20:16:57 -0300

    Step 4

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 interface CountdownProps { }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
-}
+  return (
+    <div>
+      <h1>Teste Countdown</h1>
+    </div>
+  );
+};
 
 Countdown.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
-  type: 'object',
+  title: "editor.countdown.title",
+  description: "editor.countdown.description",
+  type: "object",
   properties: {},
-}
+};
 
-export default Countdown
+export default Countdown;
